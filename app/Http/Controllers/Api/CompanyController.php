228e8cd5a9aeeed3c2d8f45d60eac737034d01da
marcos murilo <marcosmeschial@gmail.com>
2021-07-09 17:58:32 -0300

    Criando queue

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateCompany;
 use App\Http\Resources\CompanyResource;
+use App\Jobs\CompanyCreated;
 use App\Models\Company;
 use App\Services\EvaluationService;
 use Illuminate\Http\Request;
@@ -40,6 +41,8 @@ class CompanyController extends Controller
     public function store(StoreUpdateCompany $request)
     {
         $company = $this->repository->create($request->validated());
+
+        CompanyCreated::dispatch($company->email)->onQueue('queue_email');
 
         return new CompanyResource($company);
     }
--- app/Services/EvaluationService.php
+++ app/Services/EvaluationService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\Services\Traits\ConsumeExternalService;
+use Marcosfgti\MicroservicesCommon\Services\Traits\ConsumeExternalService;
 
 class EvaluationService
 {
